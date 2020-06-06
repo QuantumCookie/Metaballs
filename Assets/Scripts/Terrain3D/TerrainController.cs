4671815db83e1778d553a4f87e7326020a48f620
QuantumCookie <csksethu111@gmail.com>
2020-06-06 20:15:04 +0530

    Some progress

---
+++ Assets/Scripts/Terrain3D/TerrainController.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Terrain3D
+{
+    public class TerrainController : MonoBehaviour
+    {
+        public bool refresh = false;
+        
+        [Header("Debug Settings")]
+        public bool showDebug = true;
+        public bool showMasterBounds = true;
+        public bool showCells = true;
+        public bool showVertices = true;
+        [Range(0.01f, 0.5f)]public float vertexSize = 0.05f;
+    
+        [Header("Terrain Settings")] 
+        [Range(1, 40)]
+        public int resolution = 8;
+    
+        public float size = 10f;
+    
+        private Vector3 center;
+        private float cellSize;
+        private int power3;
+
+        private Vertex[] vertices;
+
+        private TerrainMesh terrainMesh;
+        
+        private void Start()
+        {
+            gameObject.AddComponent<MeshFilter>();
+            terrainMesh = gameObject.AddComponent<TerrainMesh>();
+            RefreshMesh();
+        }
+
+        private void RefreshMesh()
+        {
+            center = transform.position +
+                     Vector3.up * size * 0.5f +
+                     Vector3.right * size * 0.5f +
+                     Vector3.forward * size * 0.5f;
+    
+            cellSize = size / resolution;
+            power3 = resolution * resolution * resolution;
+            
+            vertices = new Vertex[power3];
+
+            for (int i = 0; i < power3; i++)
+            {
+                Vertex v = new Vertex();
+                v.k = i % resolution;
+                v.j = (i / resolution) % resolution;
+                v.i = (i / (resolution * resolution)) % resolution;
+                v.position = new Vector3(v.i * cellSize, v.j * cellSize,
+                    v.k * cellSize);
+                v.state = Calc(v.position);
+
+                vertices[i] = v;
+            }
+            
+            terrainMesh.GenerateMesh(vertices, resolution, cellSize);
+        }
+
+        private bool Calc(Vector3 pos)
+        {
+            return Random.value > 0.5f;
+            float rr = (center - pos).sqrMagnitude;
+            
+            return rr < size * size * 0.25f;
+        }
+        
+        private void OnDrawGizmos()
+        {
+            if (!showDebug) return;
+    
+            if (showMasterBounds)
+            {
+                Gizmos.color = Color.white;
+                Gizmos.DrawWireCube(center, Vector3.one * size);
+            }
+    
+            if (showCells)
+            {
+                Gizmos.color = Color.green;
+                for (int i = 0; i < resolution; i++)
+                {
+                    for (int j = 0; j < resolution; j++)
+                    {
+                        for (int k = 0; k < resolution; k++)
+                        {
+                            Vector3 position = new Vector3((i + 0.5f) * cellSize, (j + 0.5f) * cellSize,
+                                (k + 0.5f) * cellSize);
+                            Gizmos.DrawWireCube(position, Vector3.one * cellSize * 0.95f);
+                        }
+                    }
+                }
+            }
+
+            if (showVertices)
+            {
+                for (int i = 0; i < power3; i++)
+                {
+                    if (vertices[i].state) Gizmos.color = Color.black;
+                    else Gizmos.color = Color.white;
+                    
+                    Gizmos.DrawSphere(vertices[i].position, vertexSize);
+                }
+            }
+        }
+
+        private void OnValidate()
+        {
+            if (refresh)
+            {
+                refresh = false;
+                RefreshMesh();
+            }
+        }
+    }
+    
+    public class Vertex
+    {
+        public Vector3 position;
+        public int i, j, k;
+        public bool state;
+        public float value;
+    }
+}
---
+++ Assets/Scripts/Terrain3D/TerrainMesh.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Terrain3D
+{
+    public class TerrainMesh : MonoBehaviour
+    {
+        private List<Vector3> vertices;
+        private List<Vector3> normals;
+        private List<int> triangles;
+    
+        private MeshFilter meshFilter;
+        private Mesh mesh;
+    
+        private Vertex[] grid;
+        private int resolution;
+        private float cellSize;
+    
+        private void OnEnable()
+        {
+            meshFilter = GetComponent<MeshFilter>();
+            mesh = new Mesh();
+            mesh.name = "3D Terrain";
+            meshFilter.sharedMesh = mesh;
+    
+            vertices = new List<Vector3>();
+            normals = new List<Vector3>();
+            triangles = new List<int>();
+        }
+    
+        public void GenerateMesh(Vertex[] values, int _resolution, float _cellSize)
+        {
+            vertices.Clear();
+            normals.Clear();
+            triangles.Clear();
+    
+            mesh.Clear();
+    
+            grid = values;
+            resolution = _resolution;
+            cellSize = _cellSize;
+    
+            MarchingCubes();
+    
+            mesh.SetVertices(vertices);
+            mesh.SetNormals(normals);
+            mesh.SetTriangles(triangles, 0);
+        }
+    private List<Vector3> edges = new List<Vector3>();
+        private void MarchingCubes()
+        {
+            edges.Clear();
+            for (int i = 0; i < resolution - 1; i++)
+            {
+                for (int j = 0; j < resolution - 1; j++)
+                {
+                    for (int k = 0; k < resolution - 1; k++)
+                    {
+                        int mask = GetMask(i, j, k);
+                        //Debug.Log(Convert.ToString(mask, 2));
+                        edges = Triangulation.Edges(mask);
+                    }
+                }
+            }
+        }
+    
+        private int GetMask(int i, int j, int k)
+        {
+            int mask = 0;
+    
+            mask |= grid[((i + 0) * resolution + (j + 0)) * resolution + (k + 0)].state ? 1 : 0; //000
+            mask |= grid[((i + 0) * resolution + (j + 1)) * resolution + (k + 0)].state ? 2 : 0; //010
+            mask |= grid[((i + 1) * resolution + (j + 1)) * resolution + (k + 0)].state ? 4 : 0; //110
+            mask |= grid[((i + 1) * resolution + (j + 0)) * resolution + (k + 0)].state ? 8 : 0; //100
+            mask |= grid[((i + 0) * resolution + (j + 0)) * resolution + (k + 1)].state ? 16 : 0; //001
+            mask |= grid[((i + 0) * resolution + (j + 1)) * resolution + (k + 1)].state ? 32 : 0; //011
+            mask |= grid[((i + 1) * resolution + (j + 1)) * resolution + (k + 1)].state ? 64 : 0; //111
+            mask |= grid[((i + 1) * resolution + (j + 0)) * resolution + (k + 1)].state ? 128 : 0; //101
+    
+            return mask;
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            foreach (Vector3 edge in edges)
+            {
+                Gizmos.DrawSphere(edge * cellSize, 0.1f);
+            }
+        }
+    }
+}
